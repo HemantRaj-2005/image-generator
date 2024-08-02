0f1b7320f7b96cf9221c49c06a498e3d608b6240
Hemant Raj <coder.hemantraj05@gmail.com>
2024-08-02 23:09:09 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { Loader, Card, FormField } from "../components";
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+        <p>
+          Browse through a collection of imaginative 
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
